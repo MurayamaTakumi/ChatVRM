fd1a3193c36b18d2887b967d5d44774b3ab50a59
MurayamaTakumi <136677304+MurayamaTakumi@users.noreply.github.com>
2023-07-19 20:15:46 +0900

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,7 +16,7 @@ export default function Document() {
           rel="stylesheet"
         />
       </Head>
-      <body style={{ backgroundImage: `url(${buildUrl("/bg-c.png")})` }}>
+      <body style={{ backgroundImage: `url(${buildUrl("/loT3.png")})` }}>
         <Main />
         <NextScript />
       </body>
